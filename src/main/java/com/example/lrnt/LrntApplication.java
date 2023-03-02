9ad5a5f55385c16d781b81b9ace731d72f374267
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-03-02 16:59:58 +0100

    Initial commit

---
+++ src/main/java/com/example/lrnt/LrntApplication.java
@@ -0,0 +1,13 @@
+package com.example.lrnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lrnt/LrntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lrnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
