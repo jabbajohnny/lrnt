81d823a29fc2f53f37be664a4d79f7ad57339891
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-03-13 18:11:31 +0100

    Add making POST request when clicking sign up button

--- src/main/java/com/example/lrnt/MyFirstApp.java
+++ src/main/java/com/example/lrnt/MyFirstApp.java
@@ -1,13 +1,11 @@
 package com.example.lrnt;
 
 import com.example.lrnt.content.LearningAsset;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyFirstApp {
@@ -33,4 +31,6 @@ public class MyFirstApp {
     public void postAsset(@ModelAttribute LearningAsset learningAsset) {
         System.out.println("SUccess!");
     }
+
+
 }
---
+++ src/main/resources/static/javascript/login-handler.js
@@ -0,0 +1,21 @@
+document.getElementById("signup").addEventListener("submit", function (event) {
+    event.preventDefault();
+    sendData();
+});
+
+function sendData() {
+    const form = document.getElementById("signup");
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch("/api/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(r => r.json)
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+}
+
