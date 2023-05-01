178385752c851c86fab173a988ca199508e2f56b
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-05-01 18:11:19 +0200

    Random avatar generator

--- src/main/resources/static/javascript/account-board.js
+++ src/main/resources/static/javascript/account-board.js
@@ -1,6 +1,9 @@
 window.onload = function (){
     let username = document.getElementById("username");
+    let avatar = document.getElementById("avatar");
 
+    let jsonUsername;
+    let avatarSrc;
 
     fetch("/api/getUserData", {
         method: "GET",
@@ -12,8 +15,12 @@ window.onload = function (){
         .then(data => {
             const json = JSON.parse(JSON.stringify(data));
             username.innerText = json.username;
+            avatarSrc = "https://api.dicebear.com/6.x/pixel-art-neutral/svg?seed=" + json.username;
+            avatar.src = avatarSrc;
+            console.log(avatarSrc);
         })
         .catch(error => {
             console.error(error);
         });
+
 }
