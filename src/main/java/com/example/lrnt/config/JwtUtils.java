a7fd4b980931a23b8f720fbbd67dc84a9d05dd60
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-05-01 16:23:45 +0200

    Added JwtUtils class and showing content depending on being logged in or not

---
+++ src/main/java/com/example/lrnt/config/JwtUtils.java
@@ -0,0 +1,42 @@
+package com.example.lrnt.config;
+
+import com.example.lrnt.database.UserRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class JwtUtils {
+    private final JwtDecoder decoder;
+    private Jwt jwt;
+    private final UserDetailsService userDetailsService;
+    private final UserRepository repository;
+
+    public JwtUtils(JwtDecoder decoder, UserDetailsService userDetailsService, UserRepository repository) {
+        this.decoder = decoder;
+        this.userDetailsService = userDetailsService;
+        this.repository = repository;
+    }
+
+    public Authentication getAuthenticationFromJwt(String token) {
+        jwt = decoder.decode(token);
+
+        Map<String, Object> claims = jwt.getClaims();
+        UserDetails user = userDetailsService.loadUserByUsername((String) claims.get("sub"));
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return authentication;
+    }
+}
--- src/main/java/com/example/lrnt/controllers/MyFirstApp.java
+++ src/main/java/com/example/lrnt/controllers/MyFirstApp.java
@@ -1,6 +1,7 @@
 package com.example.lrnt.controllers;
 
 import com.example.lrnt.account.AccountVerifier;
+import com.example.lrnt.config.JwtUtils;
 import com.example.lrnt.database.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,26 +10,34 @@ import org.springframework.web.bind.annotation.*;
 public class MyFirstApp {
 
     private final UserRepository repository;
+    private final JwtUtils jwtUtils;
 
-    public MyFirstApp(UserRepository repository) {
+    public MyFirstApp(UserRepository repository, JwtUtils jwtUtils) {
         this.repository = repository;
+        this.jwtUtils = jwtUtils;
     }
 
 
     @RequestMapping("/")
-    public String index() {
-        return "start";
+    public String index(@CookieValue(name = "token", defaultValue = "") String token) {
+        System.out.println(token);
+        try {
+            jwtUtils.getAuthenticationFromJwt(token).isAuthenticated();
+            return "/auth/start";
+        } catch (Exception e) {
+            return "/default/start";
+        }
     }
 
     @RequestMapping("/account")
     public String account() {
-        return "account";
+        return "/default/account";
     }
 
     @RequestMapping("/confirm")
     public String confirm(@RequestParam("id") String id) {
         if (AccountVerifier.verify(id, repository).equals("confirmed")) {
-            return "confirm";
+            return "/default/confirm";
         }
         return "Error";
     }
