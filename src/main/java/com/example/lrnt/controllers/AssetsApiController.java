17c4db62b7db8a03c4947daf789fa3e98caf59cc
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-05-18 22:03:01 +0200

    Check if uploaded file is an audio file

--- src/main/java/com/example/lrnt/controllers/AssetsApiController.java
+++ src/main/java/com/example/lrnt/controllers/AssetsApiController.java
@@ -1,7 +1,9 @@
 package com.example.lrnt.controllers;
 
 import com.example.lrnt.assets.Asset;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +13,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class AssetsApiController {
 
     @PostMapping("/api/upload")
-    public ResponseEntity<JsonNode> upload(@RequestPart("file") MultipartFile file, @RequestPart("title") String title,
-                                           @RequestPart("description") String description) {
-        System.out.println(file.getName());
-        System.out.println(title);
-        System.out.println(description);
+    public ResponseEntity<JsonNode> upload(@RequestPart("file") MultipartFile file,
+                                           @RequestPart("title") String title,
+                                           @RequestPart("description") String description) throws JsonProcessingException {
+        //if file is not mp3 or any other audio format, return error
+        ObjectMapper mapper = new ObjectMapper();
+
+        if (file != null && !file.isEmpty() && !file.getContentType().equals("audio/mpeg")) {
+            JsonNode json = mapper.readTree(String.format("{\"error\": \"%s\"}", "Not an audio format!"));
+            return ResponseEntity.ok(json);
+        }
+
+        //if file is ok, save the file, create an asset, store info about asset in database table
+        Asset asset = new Asset(title, description, file);
         return null;
     }
 }
