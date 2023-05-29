49b22c843aea2c727de358a21e7f2e3d323c9971
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-05-29 20:13:25 +0200

    Return list of all assets info as a json

--- src/main/java/com/example/lrnt/controllers/AssetsApiController.java
+++ src/main/java/com/example/lrnt/controllers/AssetsApiController.java
@@ -6,6 +6,9 @@ import com.example.lrnt.database.DatabaseAsset;
 import com.example.lrnt.database.UserRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.List;
 
 @Controller
 public class AssetsApiController {
@@ -20,11 +25,13 @@ public class AssetsApiController {
     private final AssetRepository assetRepository;
     private final UserRepository userRepository;
     private final JwtUtils jwtUtils;
+    private final ObjectMapper mapper;
 
-    public AssetsApiController(AssetRepository assetRepository, UserRepository userRepository, JwtUtils jwtUtils) {
+    public AssetsApiController(AssetRepository assetRepository, UserRepository userRepository, JwtUtils jwtUtils, ObjectMapper objectMapper) {
         this.assetRepository = assetRepository;
         this.userRepository = userRepository;
         this.jwtUtils = jwtUtils;
+        this.mapper = objectMapper;
     }
 
     @PostMapping("/api/upload")
@@ -34,7 +41,6 @@ public class AssetsApiController {
                                            @CookieValue(name = "token", defaultValue = "") String token
     ) throws IOException {
         //if file is not mp3 or any other audio format, return error
-        ObjectMapper mapper = new ObjectMapper();
 
         if (file != null && !file.isEmpty() && (!file.getContentType().equals("audio/mpeg") &&
                                                !file.getContentType().equals("audio/wav"))) {
@@ -49,5 +55,27 @@ public class AssetsApiController {
 
         JsonNode json = mapper.readTree(String.format("{\"ok\": \"%s\"}", "done"));
         return ResponseEntity.ok(json);
+    }
+
+    @GetMapping("/api/assets")
+    public ResponseEntity<JsonNode> getAssets() {
+        List<DatabaseAsset> assets = assetRepository.getAll();
+        System.out.println(assets);
+
+        JsonNodeFactory factory = JsonNodeFactory.instance;
+        ArrayNode node = factory.arrayNode();
+
+        assets.forEach(asset -> {
+            ObjectNode objectNode = factory.objectNode();
+            objectNode.put("id", asset.getId());
+            objectNode.put("title", asset.getTitle());
+            objectNode.put("description", asset.getDescription());
+            objectNode.put("author", "user"); //pass users name, not id
+            objectNode.put("upload_date", asset.getUploadDate());
+
+            node.add(objectNode);
+        });
+
+        return ResponseEntity.ok(node);
     }
 }
--- src/main/java/com/example/lrnt/database/AssetRepository.java
+++ src/main/java/com/example/lrnt/database/AssetRepository.java
@@ -1,9 +1,17 @@
 package com.example.lrnt.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AssetRepository extends JpaRepository<DatabaseAsset, Long> {
     List<DatabaseAsset>  getDatabaseAssetById(String id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "SELECT * FROM lrnt.assets", nativeQuery = true)
+    List<DatabaseAsset> getAll();
 }
