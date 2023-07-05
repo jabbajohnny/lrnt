48bc4580701bcaf13d177f04ebf10d07ba7d8934
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-07-05 19:28:57 +0200

    Responsive asset cards at account page

--- src/main/resources/static/javascript/account-board.js
+++ src/main/resources/static/javascript/account-board.js
@@ -34,7 +34,6 @@ window.onload = function (){
                 const assetCard = asset.content.cloneNode(true).querySelector(".assets");
                 const title = assetCard.querySelector("[title]");
                 const description = assetCard.querySelector("[description]")
-                const author = assetCard.querySelector("[author]")
                 const uploadDate = assetCard.querySelector("[upload-date]")
                 const link = assetCard.querySelector("[link]");
 
@@ -44,7 +43,6 @@ window.onload = function (){
                     description.textContent = assetObject.description;
                 }
                 title.textContent = assetObject.title;
-                author.textContent = assetObject.author;
                 uploadDate.textContent = assetObject.upload_date;
 
                 link.href = "/" + assetObject.id;
